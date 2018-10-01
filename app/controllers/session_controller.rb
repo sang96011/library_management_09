ddb9fd29d4c62b1c5a39731471adf9bf7c70cd1c
Cindy <nhinty.nguyen@gmail.com>
2018-10-01 11:36:27 +0700

    Manage Author

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,7 +2,7 @@ class SessionController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
       flash[:info] = t("session.controller.congrate")
