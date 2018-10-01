76bc6d748003faa28948607afe491697ed519c79
Cindy <nhinty.nguyen@gmail.com>
2018-10-01 11:36:27 +0700

    sign up login logout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:info] = t "users.controller.create.welcome"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+      flash[:info] = t "users.controller.load.no_user"
+      redirect_to signup_path
+  end
+end
