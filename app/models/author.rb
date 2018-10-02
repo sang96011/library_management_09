2013737baec397c8197fd1d7b3bf1e06be953019
Van Sang Nguyen <vansang.nguyen96@gmail.com>
2018-10-02 13:25:25 +0700

    Export function

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,6 +6,15 @@ class Author < ApplicationRecord
     paginate(page: page, per_page: Settings.author.index.per_page)
   end
 
+  def self.to_xls options = {}
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |author|
+        csv << author.attributes.values_at(*column_names)
+      end
+    end
+  end
+
   validates :name, presence: true
   validates :content, presence: true
 end
